23fc1c6ab196fff0cd40c5dbd6beeb38a69da81b
pallavi kabadi <pallavi08jadhav@gmail.com>
2023-07-24 11:15:27 +0530

    Added:Basic And Fundamental Logical Programs

---
+++ src/com/bridgelabz/basicCorePrograms/PowerOfTwo.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.basicCorePrograms;
+
+import java.util.Scanner;
+
+public class PowerOfTwo {
+    public static void main(String[] args) {
+        //Take input from User
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number till that you want to print the table of 2");
+        int power = sc.nextInt();
+        if(power<=37) {
+            for (int i = 0; i <= power; i++) {
+                int ans = 2 * i;
+                System.out.println("2^" + i + "=" + ans);
+            }
+        }
+    }
+}
